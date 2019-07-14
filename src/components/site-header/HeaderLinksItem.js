df60b8e9697c56a772c33abef77e2f2b0e72a455
JonUK <jonuk2018@jjwebdesign.co.uk>
2019-07-14 12:31:43 +0100

    Show "Questions" menu item as active when on single question page

--- src/components/site-header/HeaderLinksItem.js
+++ src/components/site-header/HeaderLinksItem.js
@@ -5,9 +5,36 @@ import './HeaderLinksItem.css';
 import PropTypes from "prop-types";
 
 function HeaderLinksItem(props) {
+
+  const isActive = (match, location) => {
+
+    if (!match) {
+      return false;
+    }
+
+    if (match.isExact) {
+      return true;
+    }
+
+    // --------------------------------------------------------------------------------
+    // If the location is a match but not an exact match and the path starts with
+    // '/questions' then the root '/' route has been matched and a user is on the
+    // details page for a single question. Still consider the root route active.
+    // --------------------------------------------------------------------------------
+    if (match.isExact === false && location.pathname.startsWith('/questions')) {
+      return true;
+    }
+
+    return false;
+  };
+
   return (
     <li className="header-links-item">
-      <NavLink to={props.path} className="header-links-item__link" activeClassName="header-links-item__link--active">
+
+      <NavLink to={props.path}
+               isActive={isActive}
+               className="header-links-item__link"
+               activeClassName="header-links-item__link--active">
         {props.text}
       </NavLink>
     </li>
